02d749d36f300bee98b4751110abe4e5653f7449
Daniel Alvarado <daniell.allvarado@gmail.com>
2024-01-23 13:18:51 -0600

    se crea directorio helpers con archivo y funciones para manejo de errores

---
+++ helpers/errorHandling.js
@@ -0,0 +1,25 @@
+// Función para manejar el resultado de los procedimientos almacenados
+const handleProcedureResult = (res, result) => {
+    const { success, error, msg } = result;
+    if (error) {
+        res.status(400).json({ success, error, msg });
+    } else {
+        res.status(200).json({ success, error, msg });
+    }
+};
+
+// Función para manejar errores
+const handleError = (res, error) => {
+    console.error('Error en la operación:', error);
+    res.status(500).json({
+        success: false,
+        error: 1,
+        msg: "Error inesperado, contacte a soporte."
+    });
+};
+
+// Exportamos las funciones para manejo de errores
+module.exports = {
+    handleProcedureResult,
+    handleError
+}
